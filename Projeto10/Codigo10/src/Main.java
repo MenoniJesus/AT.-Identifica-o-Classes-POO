09834566688e12461b664961484ac642ef3d6344
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-07-16 23:42:36 -0300

    Update Main.java

--- Projeto10/Codigo10/src/Main.java
+++ Projeto10/Codigo10/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Adotante adotante1 = new Adotante("Sebas", "123456789", "Rua", "123456789", "joao@asdasd.com");
