7eabe802f280cf98a2aec1f8326b4c47480f6ed1
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-07-16 23:43:07 -0300

    Update Main.java

--- Projeto3/Codigo3/src/Main.java
+++ Projeto3/Codigo3/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Hospital hospital = new Hospital("Hospital São Paulo", "Rua do Hospital, 123", "(11) 99999-9999", 100);
