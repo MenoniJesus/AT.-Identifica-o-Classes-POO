9292e9b34e4f8ad1b719e71aed21b86f2eef4118
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-07-16 23:44:12 -0300

    Update Main.java

--- Projeto7/Codigo7/src/Main.java
+++ Projeto7/Codigo7/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Produto produto1 = new Produto("Camiseta", "Camiseta de algodão", 10, 50);
