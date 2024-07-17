223488044e5a51751c8760ed1b7913d17d15969b
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-07-16 23:43:58 -0300

    Update Main.java

--- Projeto6/Codigo6/src/Main.java
+++ Projeto6/Codigo6/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Perfil perfil1 = new Perfil("João", "foto1", "status1", "amigos1");
